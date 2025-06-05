2edb75a8bfb3a22da87b1ce36cfceefa0d30bfcb
Yehoshua Kopirin <aetente@gmail.com>
2025-06-05 16:47:00 +0300

    fix: trying to handle steering

--- src/components/SillyCar/index.tsx
+++ src/components/SillyCar/index.tsx
@@ -11,6 +11,7 @@ const SillyCar = () => {
   const threeContext = useThreeContext();
   const [car, setCar] = useState<CANNON.RaycastVehicle>(null);
   const [wheels, setWheels] = useState<CANNON.Body[]>([]);
+  const [frontWheels, setFrontWheels] = useState<CANNON.Body[]>([]);
   const [finished, setFinished] = useState(false);
 
   const carDimensions = { width: 2, height: 1, length: 5 };
@@ -25,7 +26,7 @@ const SillyCar = () => {
 
   const testMove = () => {
     if (wheels.length) {
-      console.log("MMOOOOOVVE")
+      // console.log("MMOOOOOVVE")
       const forceMagnitude = -50000;
 
       const forceVector = new CANNON.Vec3(
@@ -45,22 +46,70 @@ const SillyCar = () => {
     }
   }
 
+  const testSteer = () => {
+    if (wheels.length) {
+      console.log("STEERRRRR")
+      const eulerAngle = new CANNON.Vec3(0, Math.PI / 4, 0);
+      console.log(eulerAngle);
+      const quaternion = new CANNON.Quaternion().setFromEuler(eulerAngle.x, eulerAngle.y, eulerAngle.z);
+      console.log(quaternion);
+      frontWheels[0]?.quaternion.copy(quaternion);
+      frontWheels[1]?.quaternion.copy(quaternion);
+    }
+  }
+
   useEffect(() => {
     if (threeContext?.scene && threeContext?.world && threeContext?.startedScene && !finished && !car && !wheels.length) {
-      const body = new CANNON.Body({ mass: 100 });
+      const body = new CANNON.Body({ mass: 200 });
       const boxShape = new CANNON.Box(new CANNON.Vec3(carDimensions.width / 2, carDimensions.height / 2, carDimensions.length / 2));
       boxShape.updateConvexPolyhedronRepresentation();
       body.addShape(boxShape);
 
       body.position.set(0, 2, 0);
+      const eulerAngle = new CANNON.Vec3(0, Math.PI / 4, 0);
+      const quaternion = new CANNON.Quaternion().setFromEuler(eulerAngle.x, eulerAngle.y, eulerAngle.z);
+      body.quaternion.copy(quaternion);
+
       threeContext.world.addBody(body);
       setCar(body);
+
+      const holdFrontLeftWheel = new CANNON.Body({ mass: 10 });
+      const holdFrontRightWheel = new CANNON.Body({ mass: 10 });
+      const frontSteering = [holdFrontLeftWheel, holdFrontRightWheel];
+      frontSteering.forEach((part, i) => {
+        // part.fixedRotation = true;
+        // part.updateMassProperties();
+        const hinge = new CANNON.HingeConstraint(
+          body,
+          // new CANNON.Vec3(
+          //   wheelsPositions[i][0],
+          //   wheelsPositions[i][1] - 1.4,
+          //   wheelsPositions[i][2]
+          // ),
+          part,
+          // new CANNON.Vec3(0, 0, 0),
+          {
+            pivotA: new CANNON.Vec3(
+              wheelsPositions[i][0],
+              wheelsPositions[i][1] - 1.4,
+              wheelsPositions[i][2]
+            ),
+            pivotB: new CANNON.Vec3(0, 0, 0),
+            axisA: new CANNON.Vec3(1, 1, 1),
+            axisB: new CANNON.Vec3(1, 1, 1),
+            collideConnected: false
+          })
+        threeContext.world.addBody(part);
+        threeContext.world.addConstraint(hinge);
+      })
+      setFrontWheels(frontSteering);
+
 
       const wheelsAmount = 4;
       const wheels = [];
       for (let i = 0; i < wheelsAmount; i++) {
         const wheelShape = new CANNON.Cylinder(wheelDimensions.radius, wheelDimensions.radius, wheelDimensions.height, 32);
-        const wheelBody = new CANNON.Body({ mass: 1 });
+        const wheelBody = new CANNON.Body({ mass: 0.5 });
         wheelBody.addShape(wheelShape);
         wheels.push(wheelBody);
       }
@@ -76,14 +125,25 @@ const SillyCar = () => {
         wheel.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), Math.PI / 2);
         threeContext.world.addBody(wheel);
 
-        const hinge = new CANNON.HingeConstraint(body, wheel, {
-          pivotA: new CANNON.Vec3(wheelPosition.x, wheelPosition.y - 1.4, wheelPosition.z), // position on car body to attach to
-          pivotB: new CANNON.Vec3(0, 0, 0), // position on wheel to attach to
-          axisA: new CANNON.Vec3(1, 0, 0), // where to rotate car
-          axisB: new CANNON.Vec3(0, 0, 1), // where to rotate wheel
-          collideConnected: false,
-        });
-        threeContext.world.addConstraint(hinge);
+        if (i > 1) {
+          const hinge = new CANNON.HingeConstraint(body, wheel, {
+            pivotA: new CANNON.Vec3(wheelPosition.x, wheelPosition.y - 1.4, wheelPosition.z), // position on car body to attach to
+            pivotB: new CANNON.Vec3(0, 0, 0), // position on wheel to attach to
+            axisA: new CANNON.Vec3(1, 0, 0), // where to rotate car
+            axisB: new CANNON.Vec3(0, 0, 1), // where to rotate wheel
+            collideConnected: false,
+          });
+          threeContext.world.addConstraint(hinge);
+        } else {
+          const hinge = new CANNON.HingeConstraint(frontSteering[i], wheel, {
+            pivotA: new CANNON.Vec3(0, 0, 0), // position on car body to attach to
+            pivotB: new CANNON.Vec3(0, 0, 0), // position on wheel to attach to
+            axisA: new CANNON.Vec3(1, 0, 0), // where to rotate car
+            axisB: new CANNON.Vec3(0, 0, 1), // where to rotate wheel
+            collideConnected: false,
+          });
+          threeContext.world.addConstraint(hinge);
+        }
       });
 
       setWheels(wheels);
@@ -120,6 +180,10 @@ const SillyCar = () => {
         threeContext?.addFrameFunction(testMove);
         // testMove();
       }, 2000);
+
+      setTimeout(() => {
+        testSteer();
+      }, 6000)
 
     }
   }, [car, wheels])
